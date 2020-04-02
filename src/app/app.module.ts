b05c6828afdbcba907be3e1f3806ac87c018b338
Grace Mullins <gmullins0728@gmail.com>
2020-04-02 17:06:04 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../interfaces/todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  template: `<input [(ngModel)]="newTodo" #ctrl="ngModel" required>`
+})
+export class TodoComponent implements OnInit {
+  title = 'TODO List';
+  pTag = 'A place to store your todos';
+
+  todos: Todo[] = [
+    { task: 'do laundry', completed: false },
+    { task: 'take out trash', completed: false },
+    { task: 'groom dogs', completed: true },
+    { task: 'try to pet cat', completed: false },
+    { task: 'feed fish', completed: true },
+    { task: 'play video games', completed: true }
+  ];
+
+  
+
+  newTask = '';
+  addTask(newTask) {
+    this.todos.push({
+      task: newTask,
+      completed: false
+    });
+    this.newTask='';
+  }
+
+  completedTask(taskCompleted) {
+    taskCompleted.completed = !taskCompleted.completed;
+  }
+  
+
+  removeTask(index) {
+    for (let i = 0; i < this.todos.length; i--) {
+      if(this.todos[i].task) {
+        this.todos.splice(i, 1);
+        this.todos.splice(index, 1);
+      }
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
